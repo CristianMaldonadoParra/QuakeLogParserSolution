dddd446f7a126fffef1232642bfbad06b8b8404c
Cristian Parra <cristian.maldonado.parra@hotmail.com>
2025-07-15 17:11:52 -0300

    feat(domain): criar entidades e interfaces

---
+++ src/QuakerLogParse.Domain/Entities/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace QuakerLogParse.Domain.Entities
+{
+    public class Game
+    {
+        public string Name { get; set; } = string.Empty;
+        public int TotalKills { get; set; }
+        public List<string> Players { get; set; } = new();
+        public Dictionary<string, int> Kills { get; set; } = new();
+    }
+}
---
+++ src/QuakerLogParse.Domain/Interfaces/ILogParserService.cs
@@ -0,0 +1,9 @@
+﻿using QuakerLogParse.Domain.Entities;
+
+namespace QuakerLogParse.Domain.Interfaces
+{
+    public interface ILogParserService
+    {
+        Task<List<Game>> ParseLogAsync(string filePath);
+    }
+}
