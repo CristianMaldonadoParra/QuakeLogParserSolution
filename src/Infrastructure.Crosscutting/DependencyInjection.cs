9429ae9a502a855bc2a7063d6bbae8bcf7ff2500
Cristian Parra <cristian.maldonado.parra@hotmail.com>
2025-07-16 17:42:27 -0300

    feat(api): implementar controller e Dependency Injection

---
+++ src/Infrastructure.Crosscutting/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using QuakerLogParse.Application.Interfaces;
+using QuakerLogParse.Application.Services;
+
+namespace Infrastructure.Crosscutting
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IGameAppService, GameAppService>();
+            services.AddScoped<ILogParserAppService, LogParserAppService>();
+        }
+    }
+}
---
+++ src/QuakerLogParse.Api/Controllers/GamesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuakerLogParse.Application.Interfaces;
+
+namespace QuakerLogParse.Api.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly IGameAppService _appService;
+
+        public GamesController(IGameAppService appService)
+        {
+            _appService = appService;
+        }
+
+        [HttpGet("parse-log")]
+        public IActionResult ParseLog([FromQuery] string logFilePath)
+        {
+            var reports = _appService.GetReports(logFilePath);
+            return Ok(reports);
+        }
+    }
+}
